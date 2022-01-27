4140ab79df818c1c4382dbf0c73d041c24319da2
DESKTOP-O26PO1Q\fialv <fialvieira@gmail.com>
2022-01-27 13:54:49 -0300

    Utilização do express-validator.

--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,95 @@
+let NeDB = require('nedb');
+let db = new NeDB({
+    filename: 'users.db',
+    autoload: true
+});
+const {check} = require("express-validator");
+
 module.exports = app => {
-    app.get('/users', (req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({
-            users: [{
-                name: 'Hcode',
-                email: 'hcode@hcode.com.br',
-                id: 1
-            }]
+    let route = app.route('/users');
+
+    route.get((req, res) => {
+        db.find({}).sort({name: 1}).exec((err, users) => {
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json({
+                    users
+                });
+            }
         });
     });
 
-    app.get('/users/admin', (req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({
-            users: [{
-                admin: true,
-                name: 'Hcode',
-                email: 'hcode@hcode.com.br',
-                id: 1
-            }]
+    route.post(
+        [
+            check("name", "O nome é obrigatório.").notEmpty(),
+            check("email", "Email inválido.").notEmpty().isEmail(),
+        ],
+        (req, res) => {
+            if (!app.utils.validator.user(app, req, res)) {
+                return false;
+            }
+
+            db.insert(req.body, (err, user) => {
+                if (err) {
+                    app.utils.error.send(err, req, res);
+                } else {
+                    res.status(200).json(user);
+                }
+            });
+        });
+
+    let routeAdmin = app.route('/users/admin');
+    routeAdmin.get((req, res) => {
+        db.find({admin: 'true'}).exec((err, users) => {
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json({
+                    users
+                });
+            }
         });
     });
 
-    app.post('/users', (req, res) => {
-        res.json(req.body);
+    let routeId = app.route('/users/:id');
+    routeId.get((req, res) => {
+        db.findOne({_id: req.params.id}).exec((err, users) => {
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json({
+                    users
+                });
+            }
+        });
+    });
+
+    routeId.put(
+        [
+            check("name", "O nome é obrigatório.").notEmpty(),
+            check("email", "Email inválido.").notEmpty().isEmail(),
+        ],
+        (req, res) => {
+            if (!app.utils.validator.user(app, req, res)) {
+                return false;
+            }
+            db.update({_id: req.params.id}, req.body, err => {
+                if (err) {
+                    app.utils.error.send(err, req, res);
+                } else {
+                    res.status(200).json(Object.assign({}, req.body, req.params));
+                }
+            });
+        });
+
+    routeId.delete((req, res) => {
+        db.remove({_id: req.params.id}, {}, err => {
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json(req.params);
+            }
+        });
     });
 };
---
+++ utils/validator.js
@@ -0,0 +1,13 @@
+const {validationResult} = require("express-validator");
+module.exports = {
+    user: (app, req, res) => {
+        let errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            app.utils.error.send(errors, req, res);
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
