fef18069b53154af06c0e16ec1d4a12ffa38d98e
DESKTOP-O26PO1Q\fialv <fialvieira@gmail.com>
2022-01-26 22:36:14 -0300

    Utilizando o Consign agora.

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,26 @@
-let express = require('express');
-let routes = express.Router();
-
-routes.get('/', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-        users: [{
-            name: 'Hcode',
-            email: 'hcode@hcode.com.br',
-            id: 1
-        }]
+module.exports = app => {
+    app.get('/users', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            users: [{
+                name: 'Hcode',
+                email: 'hcode@hcode.com.br',
+                id: 1
+            }]
+        });
     });
-});
 
-routes.get('/admin', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-        users: [{
-            admin: true,
-            name: 'Hcode',
-            email: 'hcode@hcode.com.br',
-            id: 1
-        }]
+    app.get('/users/admin', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            users: [{
+                admin: true,
+                name: 'Hcode',
+                email: 'hcode@hcode.com.br',
+                id: 1
+            }]
+        });
     });
-});
-
-module.exports = routes;
+};
